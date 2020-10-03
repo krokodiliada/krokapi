8a473887f1143bcbfd1ad38be2e1167a10bd4a5e
Sergey Shilin <shilinshtein@gmail.com>
2020-10-03 15:39:37 -0400

    Initial repository setup

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    project: "./tsconfig.json",
+  },
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "jest"],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+};
