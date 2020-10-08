6d7bfb426a0d2ff060a44f402354874ee3c3b0b4
Sergey Shilin <shilinshtein@gmail.com>
2020-10-07 20:06:40 -0400

    Updated settings to be able to run Jest with typescript

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ["<rootDir>/tests"],
+  preset: "@shelf/jest-mongodb",
+  transform: { "\\.ts$": ["ts-jest"] },
+  modulePaths: ["<rootDir>/src/"],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/node_modules/"],
+};
