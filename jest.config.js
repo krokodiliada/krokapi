f64084302185ddae0ba43df146496d096b652ef7
Sergey Shilin <shilinshtein@gmail.com>
2021-02-10 10:09:13 -0500

    Initial configuration of Codecov

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-  roots: ["<rootDir>/tests"],
-  preset: "@shelf/jest-mongodb",
-  transform: { "\\.ts$": ["ts-jest"] },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,ts}"],
+  coveragePathIgnorePatterns: [
+    "<rootDir>/src/index.ts",
+    "<rootDir>/src/server.ts",
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+  ],
   modulePaths: ["<rootDir>/src/"],
+  preset: "@shelf/jest-mongodb",
+  roots: ["<rootDir>/tests"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/"],
   testTimeout: 25000,
+  transform: { "\\.ts$": ["ts-jest"] },
 };
