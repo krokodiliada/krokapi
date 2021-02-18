e78ee9c6541a5a421ab72034c72c773e74a86a39
Sergey Shilin <shilinshtein@gmail.com>
2021-02-17 23:41:31 -0500

    Created initial version of documentation

--- src/routes/v1/events.ts
+++ src/routes/v1/events.ts
@@ -1,3 +1,71 @@
+/**
+ * @openapi
+ *  components:
+ *    schemas:
+ *      Event:
+ *        type: object
+ *        required:
+ *          - number
+ *          - date
+ *        properties:
+ *          number:
+ *            type: integer
+ *            description: The consequent number of the event
+ *          categories:
+ *            type: array
+ *            description: The list of categogy IDs assigned to this event
+ *            items:
+ *              type: string
+ *          season:
+ *            type: string
+ *            description: Either spring or fall
+ *          date:
+ *            type: object
+ *            description: The date when the event starts and ends
+ *            properties:
+ *              start:
+ *                type: string
+ *                format: date
+ *                description: Start date of the event
+ *              end:
+ *                type: string
+ *                format: date
+ *                description: End date of the event
+ *          location:
+ *            type: string
+ *            description: ID of the location object
+ *          createdAt:
+ *            type: string
+ *            format: date
+ *            description: The date of the record creation.
+ *          updatedAt:
+ *            type: string
+ *            format: date
+ *            description: The date when the record was last updated.
+ *        example:
+ *           number: 50
+ *           categories: [
+ *             "5f7d04f7a54364421c7136e1",
+ *             "5f7d04f7a54364421c7136e2",
+ *             "5f7d04f7a54364421c7136e3",
+ *           ]
+ *           season: "fall"
+ *           date: {
+ *             start: "2019-09-22T04:00:00.000Z",
+ *             end: "2019-09-24T04:00:00.000Z"
+ *           }
+ *           location: "5f7d04f7a54364431c7136c5"
+ *           createdAt: "2019-09-22T06:00:00.000Z"
+ *           updatedAt: "2019-09-22T06:00:00.000Z"
+ */
+
+/**
+ * @openapi
+ *   tags:
+ *     name: Events
+ *     description: API to manage events.
+ */
+
 import express from "express";
 import EventController from "controller/Event";
 import GenericController from "controller/Common";
@@ -17,6 +85,23 @@ router.put("/", GenericController.disallowMethod);
 router.delete("/", GenericController.disallowMethod);
 
 // Router for /events/
+/**
+ * @openapi
+ *  paths:
+ *    /events:
+ *      get:
+ *        summary: Returns a list of events.
+ *        tags: [Events]
+ *        responses:
+ *          '200':
+ *            description: A JSON array of events
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  $ref: '#/components/schemas/Event'
+ *                items:
+ *                   type: string
+ */
 router.get("/", EventController.getAll);
 router.get("/:number", EventController.getByNumber);
 router.put("/:number", EventController.create);
--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,28 +1,31 @@
 import express from "express";
 
-import * as CategoryRouter from "routes/v1/categories";
-import * as CheckpointRouter from "routes/v1/checkpoints";
-import * as CheckpointAssignmentRouter from "routes/v1/checkpoint-assignments";
-import * as GpsLocationRouter from "routes/v1/locations";
-import * as EventRouter from "routes/v1/events";
-import * as ParticipantRouter from "routes/v1/participants";
-import * as RouteRouter from "routes/v1/routes";
-import * as StationRouter from "routes/v1/stations";
-import * as TagAssignmentRouter from "routes/v1/tag-assignments";
-import * as TeamRouter from "routes/v1/teams";
+import CategoryRouter from "routes/v1/categories";
+import CheckpointRouter from "routes/v1/checkpoints";
+import CheckpointAssignmentRouter from "routes/v1/checkpoint-assignments";
+import DocsRouter from "routes/v1/docs";
+import EventRouter from "routes/v1/events";
+import GpsLocationRouter from "routes/v1/locations";
+import ParticipantRouter from "routes/v1/participants";
+import RouteRouter from "routes/v1/routes";
+import StationRouter from "routes/v1/stations";
+import TagAssignmentRouter from "routes/v1/tag-assignments";
+import TeamRouter from "routes/v1/teams";
 
 // Router for /v1/ api
 const router = express.Router();
 
-router.use("/categories", CategoryRouter.default);
-router.use("/checkpoints", CheckpointRouter.default);
-router.use("/checkpoint-assignments", CheckpointAssignmentRouter.default);
-router.use("/locations", GpsLocationRouter.default);
-router.use("/events", EventRouter.default);
-router.use("/participants", ParticipantRouter.default);
-router.use("/routes", RouteRouter.default);
-router.use("/stations", StationRouter.default);
-router.use("/tag-assignments", TagAssignmentRouter.default);
-router.use("/teams", TeamRouter.default);
+// Define API routes
+router.use("/categories", CategoryRouter);
+router.use("/checkpoints", CheckpointRouter);
+router.use("/checkpoint-assignments", CheckpointAssignmentRouter);
+router.use("/docs", DocsRouter);
+router.use("/events", EventRouter);
+router.use("/locations", GpsLocationRouter);
+router.use("/participants", ParticipantRouter);
+router.use("/routes", RouteRouter);
+router.use("/stations", StationRouter);
+router.use("/tag-assignments", TagAssignmentRouter);
+router.use("/teams", TeamRouter);
 
 export default router;
